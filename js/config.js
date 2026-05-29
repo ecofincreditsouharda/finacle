89392d6b5d2787ae29504490174fb0eba42a245d
ecofincreditsouharda <ecofincreditsouharda@gmail.com>
2026-05-29 21:23:20 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,5 @@
+// =====================================
+// ECOFIN API CONFIG
+// =====================================
 
+const API_BASE_URL = "https://script.google.com/macros/s/AKfycbzcz5gkMvDofJMdHuQMp23s6GTw7LKIVyJrtoD2Gh6eXBl7tmJEEQVOMyJOOKwWmFfE/exec";
