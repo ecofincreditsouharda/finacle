1d874ea5dd14726e4d88b862ca1e889eda6b252c
ecofincreditsouharda <ecofincreditsouharda@gmail.com>
2026-05-29 21:25:36 +0530

    Update members.js

--- js/members.js
+++ js/members.js
@@ -1,1 +1,40 @@
+async function saveMember() {
 
+    const memberData = {
+
+        fullName: document.getElementById("fullName").value,
+        fatherName: document.getElementById("fatherName").value,
+        gender: document.getElementById("gender").value,
+        dob: document.getElementById("dob").value,
+        mobile: document.getElementById("mobile").value,
+        aadhaar: document.getElementById("aadhaar").value,
+        pan: document.getElementById("pan").value,
+        address: document.getElementById("address").value,
+
+        aadhaarFrontURL: "",
+        aadhaarBackURL: "",
+        panCardURL: "",
+        photoURL: ""
+
+    };
+
+    const response = await apiPost("saveMember", memberData);
+
+    if(response.status === "success") {
+
+        alert(
+            "Member Saved Successfully\n\n" +
+            "Member Number: " + response.memberNumber
+        );
+
+        location.reload();
+
+    } else {
+
+        alert("Error Saving Member");
+
+        console.log(response);
+
+    }
+
+}
