3b0b138c13abff3a960ebb230ddd7d1745938f5c
ecofincreditsouharda <ecofincreditsouharda@gmail.com>
2026-06-01 13:08:01 +0530

    Add async depositSavings function for saving deposits

--- js/savings.js
+++ js/savings.js
@@ -1,1 +1,66 @@
+async function depositSavings() {
 
+const data = {
+
+memberNumber:
+document.getElementById("memberNumber").value,
+
+amount:
+document.getElementById("amount").value,
+
+paymentMode:
+document.getElementById("paymentMode").value,
+
+remarks:
+document.getElementById("remarks").value
+
+};
+
+try {
+
+const response =
+await fetch(API_BASE_URL,{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+action:"depositSavings",
+
+data:data
+
+})
+
+});
+
+const result =
+await response.json();
+
+if(result.status==="success"){
+
+alert(
+"Deposit Successful"
+);
+
+document.getElementById("result").innerHTML=
+
+"New Balance : ₹" +
+result.balance;
+
+}else{
+
+alert(result.message);
+
+}
+
+}catch(err){
+
+alert(err);
+
+}
+
+}
